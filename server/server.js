53a736a5a5ff2130626d58f86a1da901789c3e82
andooryuu <95499752+andooryuu@users.noreply.github.com>
2025-06-07 22:19:09 -0400

    lo<jrt
    fg

--- server/server.js
+++ server/server.js
@@ -39,7 +39,7 @@ const transporter = nodemailer.createTransport({
 // Enable CORS
 app.use(
   cors({
-    origin: process.env.CORS_ORIGIN || "http://localhost:3000",
+    origin: process.env.CORS_ORIGIN,
   })
 );
 
